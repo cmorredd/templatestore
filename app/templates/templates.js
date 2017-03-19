6307275dc305e36f500854ac653905ea2c63b727
cmorredd <kumar22891@gmail.com>
2017-03-19 19:10:06 -0400

    Modified Templates

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -6,8 +6,8 @@ angular.module('templateStore.templates', ['ngRoute'])
   $routeProvider.when('/templates', {
     templateUrl: 'templates/templates.html',
     controller: 'TemplateCtrl'
-  }).
-  when('/templates/:templateId', {
+  })
+  .when('/templates/:templateId', {
     templateUrl: 'templates/template-details.html',
     controller: 'TemplateDetailsCtrl'
   })
@@ -15,26 +15,24 @@ angular.module('templateStore.templates', ['ngRoute'])
 }])
 
 .controller('TemplateCtrl', ['$scope','$http',function($scope,$http) {
-  
-   /* $http.get('json/templates.json').success(function(data){
-        console.log('Json Received');
-    })
-    .error(function(response)){
-           console.log('Error');
-           });*/
-    
-    /*$http.get('json/templates.json').then(function(response){
-        $scope.templates=response.data;
-    });*/
-    
     $http.get('json/templates.json').success(function(data){
         $scope.templates=data;
     });    
-    
-    console.log($scope);
-    
 }])
 
-.controller('TemplateDetailsCtrl', ['$scope','$http',function($scope,$http) {
-    console.log('TemplateDetailsCtrl');
+.controller('TemplateDetailsCtrl', ['$scope','$http','$routeParams','$filter',function($scope,$http,$routeParams,$filter) {
+    var templateId=$routeParams.templateId;
+    //console.log('templateId='+templateId);
+    $http.get('json/templates.json').success(function(data){
+        $scope.template=$filter('filter')(data,function(d){
+            return d.id==templateId;
+        })[0];
+        $scope.mainImage=$scope.template.images[0].name;
+        //console.log('templateName='+$scope.mainImage);
+    });
+    
+    $scope.setImage = function(image){
+		$scope.mainImage = image.name;
+	}
+    
 }]);
